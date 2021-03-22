f4ead1f9b2115e5c5241693421d304f95480b11c
kstdbrv <kstdbrv1@gmail.com>
2021-03-22 11:14:26 +0300

    WIP

--- web/sites/default/themes/custom/start_theme/assets/js/script.js
+++ web/sites/default/themes/custom/start_theme/assets/js/script.js
@@ -1,2 +1,25 @@
 
-$ = jQuery.noConflict();
+
+/* let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.menu');
+
+const toggleMenu = () => {
+  menu.classList.toggle('active');
+}
+
+hamburger.addEventListener('click', e => {
+  e.stopPropagation();
+
+  toggleMenu();
+});
+
+document.addEventListener('click', e => {
+  let target = e.target;
+  let its_menu = target == menu || menu.contains(target);
+  let its_hamburger = target == hamburger;
+  let menu_is_active = menu.classList.contains('active');
+  
+  if (!its_menu && !its_hamburger && menu_is_active) {
+    toggleMenu();
+  }
+}) */
