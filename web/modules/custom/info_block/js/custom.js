bb9380c3302a4fa003c1d14fd5df148e5e2b3d4f
kstdbrv <kstdbrv1@gmail.com>
2021-03-24 18:01:11 +0300

    works on banners

--- web/modules/custom/info_block/js/custom.js
+++ web/modules/custom/info_block/js/custom.js
@@ -1,83 +1,31 @@
-//75 лет победы
-/*function countdown(dateEnd) {
-  var timer, days, hours, minutes, seconds;
-
-  dateEnd = new Date(dateEnd);
-  dateEnd = dateEnd.getTime();
-
-  if (isNaN(dateEnd)) {
-    return;
-  }
-
-  timer = setInterval(calculate, 1000);
-
-  function calculate() {
-    var dateStart = new Date();
-    var dateStart = new Date(dateStart.getUTCFullYear(),
-      dateStart.getUTCMonth(),
-      dateStart.getUTCDate(),
-      dateStart.getUTCHours(),
-      dateStart.getUTCMinutes(),
-      dateStart.getUTCSeconds());
-    var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
-
-    if (timeRemaining >= 0) {
-      days = parseInt(timeRemaining / 86400);
-      timeRemaining = (timeRemaining % 86400);
-      hours = parseInt(timeRemaining / 3600);
-      timeRemaining = (timeRemaining % 3600);
-      minutes = parseInt(timeRemaining / 60);
-      timeRemaining = (timeRemaining % 60);
-      seconds = parseInt(timeRemaining);
-
-
-      document.getElementById("days").innerHTML = parseInt(days, 10) + ' :';
-      document.getElementById("hours").innerHTML = ("0" + hours).slice(-2) + ' :';
-      document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2) + ' :';
-      document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
-    } else {
-      return;
-    }
-  }
-
-  function display(days, hours, minutes, seconds) {}
-}
-
-countdown ('05/08/2020 09:00:00 PM');
-*/
-let pobeda75 = ( '<div class="wrap_timer">' + '<div class="timer_img"></div>' + '</div>'
-    );
-$(".oo .right_sidebar").before(pobeda75);
-
-
 
 //Национальные проекты
-let nacProject = "<div><a href='https://np.krasnodar.ru/' target='_blank' style='border: none !important;'><img id='nacProject' alt='Национальные проекты' data-align='center' data-entity-type='file' data-entity-uuid='a47168eb-26ba-44b9-85b1-16c8cabc7bc8' src='/modules/custom/info_block/images/banNacProjects.png' style='border: 1px solid #DDDDDD; margin-bottom: 30px; width: 100%; max-width: 260px; margin-left:auto; margin-right:auto;' /></a></div>";
+let nacProject = "<a 'href=https://np.krasnodar.ru/' target='_blank'><img alt='Национальные проекты' src='/modules/custom/info_block/images/banNacProjects.png'></a>";
 
 //Культурный норматив школьника
-let kultNorm = "<div><a href='/kult_normativ' style='border: none !important;'><img id='kultNorm' alt='Культурный норматив школьника' data-align='center' data-entity-type='file' data-entity-uuid='a47168eb-26ba-44b9-85b1-16c8cabc7bc8' src='/modules/custom/info_block/images/kultNorm.png' style='border: 1px solid #DDDDDD; margin-bottom: 30px; width: 100%; max-width: 260px; margin-left:auto; margin-right:auto;' /></a></div>";
+let kultNorm = "<a href='/kult_normativ'><img alt='Культурный норматив школьника' src='/modules/custom/info_block/images/kultNorm.png' /></a>";
 
 //Опрос населения
-let oprosNasel = "<div><a href='https://forms.krasnodar.ru/opros-naseleniya/?municipality=2' target='_blank' style='border: none !important;'><img id='oprosNasel' alt='Опрос населения об эффективности деятельности руководителей органов местного самоуправления' data-align='center' src='/modules/custom/info_block/images/oprosNasel.jpg' style='border: 1px solid #DDDDDD; margin-bottom: 30px; width: auto; max-width: 260px; margin-left:auto; margin-right:auto;' /></a></div>";
+let oprosNasel = "<a href='https://forms.krasnodar.ru/opros-naseleniya/?municipality=2' target='_blank'><img alt='Опрос населения об эффективности деятельности руководителей органов местного самоуправления' src='/modules/custom/info_block/images/oprosNasel.jpg' /></a>";
 
 //Опрос населения
-let otzvDist = "<div><a href='https://forms.gle/MJGcVpzpnhetTMea7' target='_blank' style='border: none !important;'><img id='otzvDist' alt='Отзыв по проведению обучения с использованием электронных и дистанционных форм обучения' data-align='center' src='/modules/custom/info_block/images/otzv_obuch.jpeg' style='border: 1px solid #DDDDDD; margin-bottom: 30px; max-width: 260px;' /></a></div>";
+let otzvDist = "<a href='https://forms.gle/MJGcVpzpnhetTMea7' target='_blank'><img alt='Отзыв по проведению обучения с использованием электронных и дистанционных форм обучения' src='/modules/custom/info_block/images/otzv_obuch.jpeg' /></a>";
 
 //Опрос населения
-//let oprosPdd = "<div><a href='https://docs.google.com/forms/d/e/1FAIpQLSdaTDE9dRT1cavKIGBLlqcAqq4LkCpWBQ0je-0zIjMOjaPxKQ/viewform?usp=sf_link' target='_blank' style='border: none !important;'><img id='oprosNasel' alt='Проверка знаний основ Правил дорожного движения' data-align='center' src='/modules/custom/goodbanners/bannersImg/pdd.jpeg' style='border: 1px solid #DDDDDD; margin-bottom: 30px; width: auto; max-width: 260px; margin-left:auto; margin-right:auto;' /></a></div>";
+/* let oprosPdd = "<a href='https://docs.google.com/forms/d/e/1FAIpQLSdaTDE9dRT1cavKIGBLlqcAqq4LkCpWBQ0je-0zIjMOjaPxKQ/viewform?usp=sf_link' target='_blank'><img alt='Проверка знаний основ Правил дорожного движения' src='/modules/custom/info_block/images/pdd.jpeg' /></a>"; */
 
 //баннер для перехода на сайт переписи населения www.strana2020.ru
-let populationCount = "<div><a href='https://www.strana2020.ru' target='_blank'><img src='/modules/custom/info_block/images/bannerPerep.jpg' alt='баннер для перехода на сайт переписи населения www.strana2020.ru' style='border: 1px solid #DDDDDD; margin-bottom: 30px; width: auto; max-width: 260px; margin-left:auto; margin-right:auto;'></a></div>";
+let populationCount = "<a href='https://www.strana2020.ru' target='_blank'><img src='/modules/custom/info_block/images/bannerPerep.jpg' alt='баннер для перехода на сайт переписи населения www.strana2020.ru' /></a>";
 
 //баннер для перехода на сайт
-let good_st = "<div><a href='http://do.krd.ru/content/%D0%B0%D0%BA%D1%86%D0%B8%D1%8F-%C2%AB%D0%B4%D0%BE%D0%B1%D1%80%D0%B0%D1%8F-%D1%81%D1%83%D0%B1%D0%B1%D0%BE%D1%82%D0%B0%C2%BB' target='_blank'><img src='/modules/custom/info_block/images/good_st.jpg' alt='баннер для перехода на сайт' style='border: 1px solid #DDDDDD; margin-bottom: 30px; width: auto; max-width: 260px; margin-left:auto; margin-right:auto;'></a></div>";
+let good_st = "<a href='http://do.krd.ru/content/%D0%B0%D0%BA%D1%86%D0%B8%D1%8F-%C2%AB%D0%B4%D0%BE%D0%B1%D1%80%D0%B0%D1%8F-%D1%81%D1%83%D0%B1%D0%B1%D0%BE%D1%82%D0%B0%C2%BB' target='_blank'><img src='/modules/custom/info_block/images/good_st.jpg' alt='баннер для перехода на сайт'/></a>";
 
-let psyConsult = "<div><a href='/psy_consult'><img alt='Консультация психолога' src='/modules/custom/info_block/images/psyConsult.jpg' style='border: 1px solid #DDDDDD; margin-bottom: 30px; width: 100%; max-width: 260px; margin-left:auto; margin-right:auto;' /></a></div>";
+let psyConsult = "<a href='/psy_consult'><img alt='Консультация психолога' src='/modules/custom/info_block/images/psyConsult.jpg'/></a>";
 
-let content = psyConsult + good_st + populationCount + otzvDist + kultNorm + nacProject + oprosNasel + pobeda75;
+let content = psyConsult + good_st + populationCount + otzvDist + kultNorm + nacProject + oprosNasel;
 
 
-$('.banners-custom').html(content);
+$('#banner-slider').children().children().children().children().children().children().html(content);
 
 
 
--- web/sites/default/themes/custom/start_theme/assets/js/script.js
+++ web/sites/default/themes/custom/start_theme/assets/js/script.js
@@ -1,26 +1,4 @@
-$ = jQuery.noConflict();
 
- /* accordeon */
- $(document).ready(function () {
-
-    $('.menu-main li.menu-main__item_collapsed>a').on('click', function(){
-        $(this).removeAttr('href');
-        let element = $(this).parent('li');
-        if (element.hasClass('active')) {
-          element.removeClass('active');
-          element.find('li').removeClass('active');
-          element.find('ul').slideUp();
-        }
-        else {
-          element.addClass('active');
-          element.children('ul').slideDown();
-          element.siblings('li').children('ul').slideUp();
-          element.siblings('li').removeClass('active');
-          element.siblings('li').find('li').removeClass('active');
-          element.siblings('li').find('ul').slideUp();
-        }
-      });
- });
 
 
 
