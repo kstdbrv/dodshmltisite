38381f48fb4e8b8292666552bfeef56c377eb8c9
kstdbrv <kstdbrv1@gmail.com>
2021-04-01 15:47:03 +0300

    add breadcrumbs

---
+++ web/sites/default/files/php/twig/6065bd0e0ecf0_container--more-link.html_xBvyCZpwZwA5dycPnj76lUpXa/8I_P-MZcvPlqqycuzWsjeLw4KJ9bLX3ml1pP02jxcMQ.php
@@ -0,0 +1,102 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* sites/default/themes/custom/start_theme/templates/content/container--more-link.html.twig */
+class __TwigTemplate_8dc8e5317d6946f483f74320a273353a49a500ad6d0d7137c7e4a0a8d104ffa7 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "
+
+<div class=\"link-more\">
+  ";
+        // line 4
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null), 4, $this->source), "html", null, true);
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "sites/default/themes/custom/start_theme/templates/content/container--more-link.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  44 => 4,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("
+
+<div class=\"link-more\">
+  {{ children }}
+</div>
+", "sites/default/themes/custom/start_theme/templates/content/container--more-link.html.twig", "/var/www/html/web/sites/default/themes/custom/start_theme/templates/content/container--more-link.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 4);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
