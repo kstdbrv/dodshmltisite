b0ab1fb1d4af1f8ff16a6bd05e639f5d19389e51
kstdbrv <kstdbrv1@gmail.com>
2021-04-20 18:01:08 +0300

    add comments block

---
+++ web/sites/default/themes/custom/start_theme/assets/js/script.js
@@ -0,0 +1,72 @@
+$ = jQuery.noConflict();
+
+$(document).ready(function () {
+  $('#block-views-block-comments .view-content').slick({
+    speed: 500,
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    adaptiveHeight: true,
+    prevArrow: '<button class="slick-prev slick-arrow"></button>',
+    nextArrow: '<button class="slick-next slick-arrow"></button>',
+     
+    /*      responsive: [
+           {
+             breakpoint: 768,
+             settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: '40px',
+               slidesToShow: 3
+             }
+           },
+           {
+             breakpoint: 480,
+             settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: '40px',
+               slidesToShow: 1
+             }
+           }
+         ] */
+  });
+
+});
+
+
+
+
+
+/* accordeon */
+//$ = jQuery.noConflict();
+ // $(document).ready(function () {
+    
+        //прикрепляем клик по заголовкам
+  //    $('.menu-header__item_collapsed').on('click', function (e) {
+  //      e.preventDefault();
+    
+  //      $(this).toggleClass('active'); /* поворот галочки */
+  //      $('.menu-header__item_collapsed').not($(this)).removeClass('active');
+       //скрываем все кроме того, что должны открыть
+  //     $('.menu-header__item_collapsed').not($(this)).find('.submenu').slideUp(300)//;
+       // открываем или скрываем блок под заголовком, по которому кликнули
+  //      $(this).find('.submenu').slideToggle(300);
+  //    });
+       
+  //  });
+
+
+/* toggle */
+// $ = jQuery.noConflict();
+/*     jQuery('.menu-header__item_collapsed').on('click', function (e) {
+      e.preventDefault();
+      
+      jQuery(this).toggleClass('active');
+      jQuery(this).find('.submenu').slideToggle(300);
+  
+  
+    }); */
+
+
+
