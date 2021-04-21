7bacc3efa12f5db260e217533a415194194724df
kstdbrv <kstdbrv1@gmail.com>
2021-04-21 17:41:44 +0300

    slider block done

--- web/sites/default/themes/custom/start_theme/assets/js/script.js
+++ web/sites/default/themes/custom/start_theme/assets/js/script.js
@@ -1,26 +1,4 @@
-$ = jQuery.noConflict();
 
-$(document).ready(function () {
-  $('#block-views-block-comments .view-content').slick({
-    speed: 500,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    adaptiveHeight: true,
-    prevArrow: '<button class="slick-prev slick-arrow"></button>',
-    nextArrow: '<button class="slick-next slick-arrow"></button>',
-          responsive: [
-           {
-             breakpoint: 768,
-             settings: {
-               arrows: false,
-               dots: true,
-             }
-           },
-         ] 
-  });
-
-});
 
 
 
--- web/sites/default/themes/custom/start_theme/assets/js/section-1.js
+++ web/sites/default/themes/custom/start_theme/assets/js/section-1.js
@@ -108,36 +108,83 @@ if (window.location.pathname === '/sveden/inter') {
 
 
 /* slick for banners */
-/* $(document).ready(function () {
+$(document).ready(function () {
 
-  setTimeout(function () {
+  if ($(window).width() > 1045) {
+
+    setTimeout(function () {
     
-    jQuery('.banners-custom').slick({
-      
-      prevArrow: '<button class="slider-banner__btn slider-banner__btn_left"><svg width="16" height="29" viewBox="0 0 16 29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.9673 14.1924L14.9422 1.37269C15.2613 1.05741 15.2613 0.551755 14.9422 0.236466C14.6231 -0.0788221 14.1113 -0.0788221 13.7922 0.236466L0.239328 13.6273C-0.0797759 13.9426 -0.0797759 14.4482 0.239328 14.7635L13.7922 28.1484C13.9487 28.3031 14.1595 28.3864 14.3642 28.3864C14.5689 28.3864 14.7796 28.309 14.9362 28.1484C15.2553 27.8331 15.2553 27.3275 14.9362 27.0122L1.9673 14.1924Z" fill="white"/></svg></button>',
-      nextArrow: '<button class="slider-banner__btn slider-banner__btn_right"><svg width="17" height="29" viewBox="0 0 17 29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.8936 13.632L2.33608 0.236548C2.01686 -0.0788495 1.50491 -0.0788495 1.1857 0.236548C0.866485 0.551946 0.866485 1.05777 1.1857 1.37317L14.1651 14.1974L1.1857 27.0216C0.866485 27.337 0.866485 27.8428 1.1857 28.1582C1.3423 28.3129 1.5531 28.3962 1.75788 28.3962C1.96266 28.3962 2.17346 28.3189 2.33005 28.1582L15.8876 14.7627C16.2068 14.4533 16.2068 13.9415 15.8936 13.632Z" fill="white"/></svg></button>',
-      infinite: true,
-      rtl: true справо налево движение слайдера
-      vertical: true,
-      adaptiveHeight: true,
-      verticalSwiping: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 0,
-      speed: 3000,
-      arrows: false,
-      variableHight: true,
-      responsive: [
-        {
-          breakpoint: 969,
-          settings: {
-           arrows: false
+      jQuery('.banners-custom').slick({
+        /*       prevArrow: '<button class="slick-prev slick-arrow"></button>',
+              nextArrow: '<button class="slick-next slick-arrow"></button>', */
+        speed: 500,
+        infinite: true,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        vertical: true,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        verticalSwiping: true,
+        responsive: [
+          {
+            breakpoint: 1047,
+            settings: "unslick"
           }
-        },
-      ]
-    });
+        ]
+      });
 
-}, 200);
-}); */
+        // mousewheel on   // прокрутка мышкой
+       let slider = $(".banners-custom");
+
+       slider.on('wheel', (function (e) {
+        e.preventDefault();
+
+       if (e.originalEvent.deltaY < 0) {
+         $(this).slick('slickNext');
+       } else {
+         $(this).slick('slickPrev');
+       }
+      }));
+
+    }, 500);
+  }
+
+
+
+
+
+  if ($(window).width() < 1045) {
+  
+    setTimeout(function () {
+  
+      jQuery('.banners-custom').slick({
+        speed: 500,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        arrows: false,
+        dots: true,
+            responsive: [
+           {
+            breakpoint: 995,
+             settings: {
+              slidesToShow: 2,
+             }
+            },
+            {
+            breakpoint: 700,
+             settings: {
+              slidesToShow: 1,
+              adaptiveHeight: true,
+             }
+            },
+        ]
+      });
+  
+    }, 500);
+  }
+
+});
