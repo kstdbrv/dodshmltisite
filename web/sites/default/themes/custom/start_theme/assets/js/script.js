c0e843dab2bf1cd41e11793822127dbd993f4305
kstdbrv <kstdbrv1@gmail.com>
2021-04-23 13:11:30 +0300

    remore gallery done

--- web/sites/default/themes/custom/start_theme/assets/js/script.js
+++ web/sites/default/themes/custom/start_theme/assets/js/script.js
@@ -1,8 +1,25 @@
-
+$ = jQuery.noConflict();
 // инициализируем анамацию появления блоков
 // https://wowjs.uk/docs.html
 new WOW().init();
 //https://animate.style/  - эффекты
+
+
+$(window).scroll(function () {
+     // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+     if ($(this).scrollTop() > 50) {
+         $('.button-up').fadeIn();
+     } else {
+         $('.button-up').fadeOut();
+     }
+ });
+ /** При нажатии на кнопку мы перемещаемся к началу страницы */
+ $('.button-up').click(function () {
+     $('body,html').animate({
+         scrollTop: 0
+     }, 500);
+     return false;
+ });
 
 
 
