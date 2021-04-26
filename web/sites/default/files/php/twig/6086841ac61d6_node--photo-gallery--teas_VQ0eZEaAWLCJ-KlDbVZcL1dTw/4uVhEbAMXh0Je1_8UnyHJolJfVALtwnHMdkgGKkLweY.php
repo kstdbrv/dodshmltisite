69ca463229bd0c9be4a85cb8c60ebd791f8b91d2
kstdbrv <kstdbrv1@gmail.com>
2021-04-26 12:17:53 +0300

    staff done

---
+++ web/sites/default/files/php/twig/6086841ac61d6_node--photo-gallery--teas_VQ0eZEaAWLCJ-KlDbVZcL1dTw/4uVhEbAMXh0Je1_8UnyHJolJfVALtwnHMdkgGKkLweY.php
@@ -0,0 +1,264 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* sites/default/themes/custom/start_theme/templates/content/node--photo-gallery--teaser.html.twig */
+class __TwigTemplate_ee6254d5725b5f5ee3d4d65fccca611b4f071881853d9b09f313cd6e02cf4338 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 70
+        $context["classes"] = [0 => "photo-gallery", 1 => "node", 2 => ("node-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source,         // line 73
+($context["node"] ?? null), "bundle", [], "any", false, false, true, 73), 73, $this->source))), 3 => ((        // line 74
+($context["view_mode"] ?? null)) ? (("node--view-mode-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null), 74, $this->source)))) : (""))];
+        // line 77
+        echo "
+<div";
+        // line 78
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 78), 78, $this->source), "html", null, true);
+        echo ">
+
+  <div class=\"photo-gallery__image\">
+    ";
+        // line 81
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed((($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4 = twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_photo_image", [], "any", false, false, true, 81)) && is_array($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4) || $__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4 instanceof ArrayAccess ? ($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4[0] ?? null) : null), 81, $this->source), "html", null, true);
+        echo "
+  </div>
+
+  <div class=\"photo-gallery__wrapper\">
+
+    <div class=\"photo-gallery__title\">
+    ";
+        // line 87
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 87, $this->source), "html", null, true);
+        echo "
+    </div>
+
+    <div class=\"photo-gallery__bottom\">
+      <div class=\"news-teaser__statistics\">
+        ";
+        // line 92
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["statistics"] ?? null), 92, $this->source), "html", null, true);
+        echo "
+      </div>
+      <div class=\"photo-gallery__created\">
+        <span>";
+        // line 95
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, twig_date_format_filter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["node"] ?? null), "getCreatedTime", [], "method", false, false, true, 95), 95, $this->source), "d.m.Y"), "html", null, true);
+        echo "</span>  
+      </div>
+    </div>
+
+  </div>
+  <a href=\"";
+        // line 100
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url"] ?? null), 100, $this->source), "html", null, true);
+        echo "\" class=\"photo-gallery__link\">
+    <span class=\"photo-gallery__hover-text\">
+     ";
+        // line 102
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_photo_text", [], "any", false, false, true, 102), 102, $this->source), "html", null, true);
+        echo "
+    </span>
+  </a>
+  
+</div>
+
+";
+        // line 110
+        echo "
+";
+        // line 113
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "sites/default/themes/custom/start_theme/templates/content/node--photo-gallery--teaser.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  100 => 113,  97 => 110,  88 => 102,  83 => 100,  75 => 95,  69 => 92,  61 => 87,  52 => 81,  46 => 78,  43 => 77,  41 => 74,  40 => 73,  39 => 70,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Bartik's theme implementation to display a node.
+ *
+ * Available variables:
+ * - node: The node entity with limited access to object properties and methods.
+ *   Only method names starting with \"get\", \"has\", or \"is\" and a few common
+ *   methods such as \"id\", \"label\", and \"bundle\" are available. For example:
+ *   - node.getCreatedTime() will return the node creation timestamp.
+ *   - node.hasField('field_example') returns TRUE if the node bundle includes
+ *     field_example. (This does not indicate the presence of a value in this
+ *     field.)
+ *   - node.isPublished() will return whether the node is published or not.
+ *   Calling other methods, such as node.delete(), will result in an exception.
+ *   See \\Drupal\\node\\Entity\\Node for a full list of public properties and
+ *   methods for the node object.
+ * - label: (optional) The title of the node.
+ * - content: All node items. Use {{ content }} to print them all,
+ *   or print a subset such as {{ content.field_example }}. Use
+ *   {{ content|without('field_example') }} to temporarily suppress the printing
+ *   of a given child element.
+ * - author_picture: The node author user entity, rendered using the \"compact\"
+ *   view mode.
+ * - metadata: Metadata for this node.
+ * - date: (optional) Themed creation date field.
+ * - author_name: (optional) Themed author name field.
+ * - url: Direct URL of the current node.
+ * - display_submitted: Whether submission information should be displayed.
+ * - attributes: HTML attributes for the containing element.
+ *   The attributes.class element may contain one or more of the following
+ *   classes:
+ *   - node: The current template type (also known as a \"theming hook\").
+ *   - node--type-[type]: The current node type. For example, if the node is an
+ *     \"Article\" it would result in \"node--type-article\". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node--view-mode-[view_mode]: The View Mode of the node; for example, a
+ *     teaser would result in: \"node--view-mode-teaser\", and
+ *     full: \"node--view-mode-full\".
+ *   The following are controlled through the node publishing options.
+ *   - node--promoted: Appears on nodes promoted to the front page.
+ *   - node--sticky: Appears on nodes ordered above other non-sticky nodes in
+ *     teaser listings.
+ *   - node--unpublished: Appears on unpublished nodes visible only to site
+ *     admins.
+ * - title_attributes: Same as attributes, except applied to the main title
+ *   tag that appears in the template.
+ * - content_attributes: Same as attributes, except applied to the main
+ *   content tag that appears in the template.
+ * - author_attributes: Same as attributes, except applied to the author of
+ *   the node tag that appears in the template.
+ * - title_prefix: Additional output populated by modules, intended to be
+ *   displayed in front of the main title tag that appears in the template.
+ * - title_suffix: Additional output populated by modules, intended to be
+ *   displayed after the main title tag that appears in the template.
+ * - view_mode: View mode; for example, \"teaser\" or \"full\".
+ * - teaser: Flag for the teaser state. Will be true if view_mode is 'teaser'.
+ * - page: Flag for the full page state. Will be true if view_mode is 'full'.
+ * - readmore: Flag for more state. Will be true if the teaser content of the
+ *   node cannot hold the main body content.
+ * - logged_in: Flag for authenticated user status. Will be true when the
+ *   current user is a logged-in member.
+ * - is_admin: Flag for admin user status. Will be true when the current user
+ *   is an administrator.
+ *
+ * @see template_preprocess_node()
+ */
+#}
+{%
+  set classes = [
+    'photo-gallery',
+    'node',
+    'node-' ~ node.bundle|clean_class,
+    view_mode ? 'node--view-mode-' ~ view_mode|clean_class,
+  ]
+%}
+
+<div{{ attributes.addClass(classes) }}>
+
+  <div class=\"photo-gallery__image\">
+    {{ content.field_photo_image[0] }}
+  </div>
+
+  <div class=\"photo-gallery__wrapper\">
+
+    <div class=\"photo-gallery__title\">
+    {{ label }}
+    </div>
+
+    <div class=\"photo-gallery__bottom\">
+      <div class=\"news-teaser__statistics\">
+        {{ statistics }}
+      </div>
+      <div class=\"photo-gallery__created\">
+        <span>{{ node.getCreatedTime()|date(\"d.m.Y\") }}</span>  
+      </div>
+    </div>
+
+  </div>
+  <a href=\"{{ url }}\" class=\"photo-gallery__link\">
+    <span class=\"photo-gallery__hover-text\">
+     {{ content.field_photo_text }}
+    </span>
+  </a>
+  
+</div>
+
+{# {{ content|without('field_image_news') }} #}
+{# {% if pathToPhoto|length == null %} #}
+
+{# Лучше всего его через препроцесс или ещё как вбросить туда
+\$entity->get('FIELD_WITH_COMMENTS')->comment_count #}
+
+", "sites/default/themes/custom/start_theme/templates/content/node--photo-gallery--teaser.html.twig", "/var/www/html/web/sites/default/themes/custom/start_theme/templates/content/node--photo-gallery--teaser.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 70);
+        static $filters = array("clean_class" => 73, "escape" => 78, "date" => 95);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['clean_class', 'escape', 'date'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
